a5c492e1c2cedc1563a5fdf60a0f8cbaa3c47d53
Moonlight Blessed <moonlightblessed6@gmail.com>
2026-01-21 01:19:51 -0800

    first commit

--- src/Componient/Navbar/Navbar.js
+++ src/Componient/Navbar/Navbar.js
@@ -16,6 +16,15 @@ top: 0;
 left: 0;
 z-index: 9999;
 
+.dropdown-icon{
+    display: none;
+}
+
+
+.myurls{
+    display: none;
+}
+
 .Nav-bar{
     display: flex;
     justify-content: space-between;
@@ -74,5 +83,46 @@ font-size: 16px;
 
 .navright button:hover{
     background-color: #106f9e;
+}
+
+
+@media screen and (max-width: 815px) {
+    
+    .navmiddle, .navright{
+        display: none;
+    }
+
+
+    .dropdown-icon{
+        display: block;
+    }
+
+    svg{
+        font-size: 30px;
+    }
+
+    .mobilesview{
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        right: 0;
+        top: 58px;
+        
+        border-bottom-left-radius: 10px;
+    }
+
+    .myurls{
+        display: flex;
+        flex-direction: column;
+         padding: 20px 20px;
+        background-color: #f2faff;
+border-bottom-left-radius: 10px;
+        
+    }
+
+    .navright{
+        color: yellow;
+    }
+    
 }
 `
--- src/Componient/Navbar/Navbar.jsx
+++ src/Componient/Navbar/Navbar.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import {Nav} from './Navbar'
 import Logo from '../../assets/Logo/logo.png'
 import { IoIosPersonAdd } from "react-icons/io";
+import { RiMenu3Fill , RiCloseLargeFill} from "react-icons/ri";
+
+
+
 
 
 
 
 
 const Navbar = () => {
+
+    const [dropmenue, setDropmenue] = useState(false)
+
+    const dropdwn = () => {
+        setDropmenue(prev => !prev)
+    }
 
     return(
         <Nav>
@@ -16,6 +26,30 @@ const Navbar = () => {
              <img src={Logo} alt="Logo" />
             </div>
 
+          {dropmenue &&(
+            <div className="mobilesview">
+               <ul className="myurls">
+                <li onClick={() => window.location.href = '/'}>Home</li>
+                <li onClick={() => window.location.href = '/about-us'}>About</li>
+                <li onClick={() => window.location.href = '/our-service'}>Services</li>
+                <li onClick={() => window.location.href = '/contact-us'}>Contact</li>
+                <li onClick={() => window.location.href = '/login'}>Login</li>
+              <li style={{display: 'flex', justifyContent:'center', alignItems: 'center', gap: '3px'}} onClick={() => window.location.href = '/open-account'}>Open Account</li>
+       
+             </ul>
+             
+            </div>
+          )}
+ 
+<div className="dropdown-icon">
+  {dropmenue ? (
+    <RiCloseLargeFill onClick={dropdwn} />
+  ) : (
+    <RiMenu3Fill onClick={dropdwn} />
+  )}
+</div>
+
+
             <div className="navmiddle">
              <ul>
                 <li onClick={() => window.location.href = '/'}>Home</li>
@@ -23,6 +57,7 @@ const Navbar = () => {
                 <li onClick={() => window.location.href = '/our-service'}>Services</li>
                 <li onClick={() => window.location.href = '/contact-us'}>Contact</li>
              </ul>
+
             </div>
 
             <div className="navright">
